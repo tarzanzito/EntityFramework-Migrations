375f93a60adab8f8482fde534fe9a6df6261371e
tarzanzito <tarzanzito@gmail.com>
2021-11-25 11:06:29 +0000

    Main function

--- Example2_DotNet6/Program.cs
+++ Example2_DotNet6/Program.cs
@@ -1,43 +1,27 @@
 
-using Migration2.Models;
+///////////////////////////////////
+//Top-level statements  (concept)
+///////////////////////////////////
+
 using Microsoft.EntityFrameworkCore;
 using Migration2.Data;
-using Migration2;
 
-namespace Migration2
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //dotnet ef migrations add myInitialCreation
-            //dotnet ef database update
+//dotnet ef migrations add myInitialCreation
+//dotnet ef database update
 
-            Console.WriteLine("Starting like a Wrb API");
+Console.WriteLine("Starting like a Wrb API");
 
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            string connString = builder.Configuration["ConnectionStrings:DefaultConnection3"];
+string connString = builder.Configuration["ConnectionStrings:DefaultConnection3"];
 
-            builder.Services.AddDbContext<AppDbContext>(
-              options =>
-              {
-                  options.UseSqlite(connString);
-                  options.LogTo(Console.WriteLine);
-              }
-              );
-
-            var app = builder.Build();
-
-            //not need for migration
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
-            //app.UseHttpsRedirection();
-
-            app.Run();
-        }
+builder.Services.AddDbContext<AppDbContext>(
+    options => {
+        options.UseSqlite(connString);
+        options.LogTo(Console.WriteLine);
     }
-}
+);
+
+var app = builder.Build();
+
+app.Run();
